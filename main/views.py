b91c1c2ad7a6b2e0c449abdf4e7bc8381c3678b2
furkansimsekli <simseklifurkan0@gmail.com>
2023-07-26 15:06:19 +0300

    refactor: cleanup UIKit from javascript

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 import base64
+import uuid
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
     return render(request, 'main/index.html')
 
@@ -13,9 +13,8 @@ def save_audio(request):
     data_str = request.body.decode('ascii')
     data = json.loads(data_str)
     audio = data['audio']
-    title = data.get('title')
-    wav_file = open(f"{title}.mp3", "wb")
+    title = f'record_{str(uuid.uuid4())[:8]}'
+    wav_file = open(f'records/{title}.mp3', 'wb')
     decode_string = base64.b64decode(audio)
     wav_file.write(decode_string)
-
     return HttpResponse('Saved')
